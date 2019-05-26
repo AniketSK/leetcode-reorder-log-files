b325373fa945b2fa139b417dddbafee65d3af88f
Aniket Kadam <anikadamg@gmail.com>
2019-05-26 11:19:15 +0530

    Decided to implement sorting via compare function
    Added rules and tests as part of that.

--- reorder-logs.js
+++ reorder-logs.js
@@ -1,9 +1,35 @@
 /**
+ * Returns -1 if b should be before a, 0 if equal, 1 if a should be before b.
+ * Rules via leetcode:
+ *  1. All logs that begin with alphabets should be before any string with a number.
+ *         Note: this still ignores identifiers (the first word) and considers second only
+ *  2. Alphabet strings should be sorted lexicographically (alphebatized) but igoring the
+ *         first word, which remains the identifier except:
+ *  3. If two letter logs tie, then the identifier is used to order them.
+ * @param {string} a one item to compare
+ * @param {string} b second item that you're comparing with
+ */
+function compareFunction(a, b)
+{
+    return a - b
+}
+
+/**
+ * Given an entire log of the type 'g1 act car', returns whether it's a number log or not.
+ * Eg: true for "zo4 4 7"
+ * false for "g1 act car"
+ */
+function isNumberLog(log) {
+
+}
+
+/**
  * @param {string[]} logs
  * @return {string[]}
  */
 function reorderLogFiles(logs) {
-
+    return logs.sort(compareFunction);
 };
 
-module.exports = reorderLogFiles
+
+module.exports = { reorderLogFiles, isNumberLog }
--- reorder-logs.test.js
+++ reorder-logs.test.js
@@ -1,9 +1,17 @@
-const reorderLogFiles = require('./reorder-logs');
+const { reorderLogFiles, isNumberLog } = require('./reorder-logs');
 
 let sampleInput = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
 let expectedOutput = ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
 
-test('Gets the expected output', () => {
+test('isNumberLog correctly identifies alphabet logs' , () => {
+    expect(isNumberLog("g1 act car")).toBe(false)
+})
+
+test.skip('isNumberLog correctly identifies for number logs' , () => {
+    expect(isNumberLog("zo4 4 7")).toBe(true)
+})
+
+test.skip('Gets the expected output', () => {
     let result = reorderLogFiles(sampleInput)
-    expect(result).toBe(expectedOutput)
+    expect(result).toEqual(expectedOutput)
 })
